ca068002b62031cf6fb979c4fc73b851eca2795b
Gene Taylor <gtaylor@atlassian.com>
2017-06-17 13:30:54 +1000

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # gtt-blog
+
+blog setup
+stack new hakyll-template
+stack init
+stack install hakyll-sass
+add hakyll-sass to extra deps
+
+
+* stack build
+Builds the executable, required if you make changes to site.hs
+
+* stack exec gtt-blog rebuild
+rebuilds the entire site
+
+* stack exec gtt-blog watch
+builds the site, serves it on localhost:8000, and watches it for changes to automatically rebuild it
--- site.hs
+++ site.hs
@@ -3,15 +3,31 @@
 import           Data.Monoid (mappend)
 import           Hakyll
 import           Hakyll.Web.Sass (sassCompiler)
+import           Hakyll.Web.Redirect (createRedirects)
 ---------------------------------------------------------------------------
+
+base_url :: String
+base_url = "http://gene-t-taylor.com"
+
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    dateField "dateISO" "%Y-%m-%dT%H:%M:%S" `mappend`
+    constField "base_url" base_url `mappend`
+    defaultContext
+
 
 main :: IO ()
 main = hakyll $ do
+
+    -- create static redirect pages for outdated/broken incoming links (goes first so any collisions with content, the redirects will lose)
+    version "redirects" $ createRedirects brokenLinks
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match (fromList ["about.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
@@ -38,6 +54,20 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
+    create ["sitemap.xml"] $ do
+         route   idRoute
+         compile $ do
+           posts <- recentFirst =<< loadAll "posts/*"
+           about <- load "about.markdown"
+           archive <- load "archive.html"
+           index <- load "index.html"
+           let allPosts = (return (posts ++ [about, archive, index]))
+           let sitemapCtx = listField "entries" postCtx allPosts  `mappend`
+                            constField "host" base_url            `mappend`
+                            defaultContext
+           makeItem ""
+            >>= loadAndApplyTemplate "templates/sitemap.xml" sitemapCtx
+            >>= relativizeUrls
 
     match "index.html" $ do
         route idRoute
@@ -60,11 +90,14 @@ main = hakyll $ do
         let compressCssItem = fmap compressCss
         compile (compressCssItem <$> sassCompiler)
 
---------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    dateField "dateISO" "%Y-%m-%dT%H:%M:%S" `mappend`
-    constField "base_url" "http://gene-t-taylor.com/" `mappend`
-    defaultContext
+-------------------------------------------------------------------------------- 
 
+--   For use with `createRedirects`.
+brokenLinks :: [(Identifier,FilePath)]
+brokenLinks = [
+   ("2016/11/13/mockito-varargs-matcher/index.html", "/posts/2016-11-13-mockito-varargs-matcher.html")
+ , ("2016/11/07/haskell-course/index.html", "/posts/2016-11-07-haskell-course.html")
+ , ("2016/07/20/CompletableFuture-sequence/index.html", "/posts/2016-07-20-CompletableFuture-sequence.html")
+ , ("2016/06/28/pattern-matching-in-java/index.html", "/posts/2016-06-28-pattern-matching-in-java.html")
+ , ("2016/03/26/guava-immutablemap-collector/index.html", "/posts/2016-03-26-guava-immutablemap-collector.html")
+ ]
