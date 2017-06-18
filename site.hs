b66edbaa420978d34a132c91c8a27d4e863aa1e1
Gene Taylor <gtaylor@atlassian.com>
2017-06-18 19:43:18 +1000

    WIP

--- site.hs
+++ site.hs
@@ -8,7 +8,8 @@ import           Hakyll.Web.Redirect (createRedirects)
 ---------------------------------------------------------------------------
 
 base_url :: String
-base_url = "http://gene-t-taylor.com"
+-- base_url = "http://gene-t-taylor.com"
+base_url = "http://127.0.0.1:8000"
 
 siteCtx :: Context String
 siteCtx = constField "site.title" "siteTitlePlaceholder" `mappend`
@@ -23,11 +24,10 @@ postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     dateField "dateISO" "%Y-%m-%dT%H:%M:%S" `mappend`
     constField "base_url" base_url `mappend`
-    listField "recentPosts" postCtx (loadAllSnapshots "pages/*" "forListing") `mappend`
+    listField "recentPosts" postCtx recentPosts `mappend`
+    -- listField "recentPosts" postCtx ((loadAllSnapshots "pages/*" "forListing") >>= \x -> pure(take 3 x)) `mappend`
     siteCtx `mappend`
     defaultContext
-
-
 
 -- tags <- buildTags "posts/*" (fromCapture "tags/*.html") 
 
@@ -51,6 +51,13 @@ destination = setVersion Nothing <$> getUnderlying
     --         recent <- recentFirst all
     --         recentPosts <- pure(take 3 recent)
 
+recentPosts :: Compiler [Item String]
+recentPosts  = do
+    identifiers <- getMatches "posts/*"
+    -- saveSnapshot "forListing"
+    ordered <- recentFirst $ [Item identifier "" | identifier <- identifiers]
+    return (take 3 ordered)
+
 main :: IO ()
 main = hakyll $ do
 
@@ -61,25 +68,25 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
-    -- match (fromList ["about.markdown"]) $ do
-    --     route   $ setExtension "html"
-    --     compile $ pandocCompiler
-    --         >>= loadAndApplyTemplate "templates/post.html" postCtx
-    --         >>= relativizeUrls
+    match "about.markdown" $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/page.html" (constField "noSocial" "true" `mappend` postCtx)
+            >>= relativizeUrls
     
     -- compileMenu 
 
     -- let rrecentPosts = compile $ do
     --         (recentFirst =<< loadAll "posts/*") >>= pure(take 3 allPosts)
 
-    match "posts/*" $ version "forListing" $ do
-        compile $ getResourceString 
+    -- match "posts/*" $ version "forListing" $ do
+    --     compile $ getResourceString >>= saveSnapshot "forListing"
 
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ do 
             -- let indexCtx =  listField "recentPosts" postCtx (return rrecentPosts) `mappend`
-            --                 postCtx 
+            --                   ((listField "recentPosts" postCtx fakePosts) `mappend` postCtx)
             pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html" postCtx
             >>= relativizeUrls
@@ -105,18 +112,18 @@ main = hakyll $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            allPosts <- recentFirst =<< (loadAll ("posts/*" .&&. hasVersion "forListing"))
-            recentPosts <- pure(take 3 allPosts)
-            let indexCtx =  listField "allPosts" postCtx (return allPosts) `mappend`
-                            listField "recentPosts" postCtx (return recentPosts) `mappend`
-                            constField "title" "Recent Posts" `mappend`
-                            siteCtx `mappend`
-                            dateField "date" "%B %e, %Y" `mappend`
-                            dateField "dateISO" "%Y-%m-%dT%H:%M:%S" `mappend`
-                            constField "base_url" base_url `mappend`
-                            defaultContext
+            -- allPosts <- recentFirst =<< (loadAll ("posts/*" .&&. hasVersion "forListing"))
+            -- recentPosts <- pure(take 3 allPosts)
+            -- let indexCtx =  listField "allPosts" postCtx (return allPosts) `mappend`
+            --                 listField "recentPosts" postCtx (return recentPosts) `mappend`
+            --                 constField "title" "Recent Posts" `mappend`
+            --                 siteCtx `mappend`
+            --                 dateField "date" "%B %e, %Y" `mappend`
+            --                 dateField "dateISO" "%Y-%m-%dT%H:%M:%S" `mappend`
+            --                 constField "base_url" base_url `mappend`
+            --                 defaultContext
             pandocCompiler
-                >>= loadAndApplyTemplate "templates/home.html" indexCtx
+                >>= loadAndApplyTemplate "templates/home.html" postCtx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
@@ -127,8 +134,15 @@ main = hakyll $ do
         let compressCssItem = fmap compressCss
         compile (compressCssItem <$> sassCompiler)
 
-    -- match "assets/css/entypo.css" $ do
-    --     route $ constRoute "css/entypo.css" 
+    match ( "assets/fonts/*"
+            .||. "assets/js/*" 
+            .||. "assets/js/vendor/*" 
+            .||. "assets/*.png"
+            .||. "assets/*.ico" 
+            .||. "assets/css/fonts/*" 
+            .||. "assets/css/entypo.css") $ do
+          route   idRoute
+          compile copyFileCompiler       
 
 -------------------------------------------------------------------------------- 
 
